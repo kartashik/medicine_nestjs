2b5f3d67ce55b98a8621c45991abc0aba9538081
foryucheba5 <forycheba5@gmail.com>
2022-11-19 13:52:05 +0400

    Бекенд протоколов и паттернов

--- src/protocol/protocols.module.ts
+++ src/protocol/protocols.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ProtocolsService } from './protocols.service';
 import { ProtocolsController } from './protocols.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthModule } from 'src/auth/auth.module';
+import { Protocol } from './protocols.model';
 
 @Module({
   providers: [ProtocolsService],
-  controllers: [ProtocolsController]
+  controllers: [ProtocolsController],
+  imports: [
+    SequelizeModule.forFeature([Protocol]),
+    forwardRef(() => AuthModule),
+  ],
+  exports: [
+    ProtocolsService,
+  ]
 })
 export class ProtocolsModule {}
