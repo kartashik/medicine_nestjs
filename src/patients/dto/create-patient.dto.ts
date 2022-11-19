0dfc99c7ac818539fc096d54453356698f3e955e
Semenova_Elizaveta <merlizaveta@gmail.com>
2022-11-19 13:41:30 +0400

    бекенд пациента

---
+++ src/patients/dto/create-patient.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
+
+export class CreatePatientDto{
+  @ApiProperty({example: "Семёнова", description: 'Фамилия пользователя'})
+  @IsString({message: "Должно быть строкой"})
+  readonly secondName: string;
+  @ApiProperty({example: "Елизавета", description: 'Имя пользователя'})
+  @IsString({message: "Должно быть строкой"})
+  readonly firstName: string;
+  @ApiProperty({example: "Андреевна", description: 'Отчество пользователя'})
+  @IsString({message: "Должно быть строкой"})
+  readonly middleName: string;
+  @ApiProperty({example: "1234 567890", description: 'Паспорт пользователя'})
+  @IsString({message: "Должно быть строкой"})
+  readonly passport: string;
+  @ApiProperty({example: "2000.12.20", description: 'Дата рождения пользователя'})
+  @IsString({message: "Должно быть датой"})
+  readonly dateOfBirth: Date;
+  @ApiProperty({example: "1", description: 'Пол пользователя 1 - м, 0 - ж'})
+  @IsString({message: "Должно быть числом"})
+  readonly gender: number;
+  @ApiProperty({example: "89096387223", description: 'Телефон пользователя'})
+  @IsString({message: "Должно быть строкой"})
+  readonly phone: string;
+  @ApiProperty({example: "1234 567890", description: 'Уникальный идентификатор пользователя'})
+  @IsString({message: "Должно быть числом"})
+  readonly userId: number;
+}
--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthModule } from 'src/auth/auth.module';
 import { PatientsController } from './patients.controller';
+import { Patient } from './patients.model';
 import { PatientsService } from './patients.service';
 
 @Module({
   controllers: [PatientsController],
-  providers: [PatientsService]
+  providers: [PatientsService],
+  imports: [
+    SequelizeModule.forFeature([Patient]),
+    forwardRef(() => AuthModule),
+  ],
+  exports: [
+    PatientsService,
+  ]
 })
 export class PatientsModule {}
