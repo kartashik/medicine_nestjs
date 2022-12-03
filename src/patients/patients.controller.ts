a49f45fdca2920a18eadb4fbd0ff00445ea17448
Semenova_Elizaveta <merlizaveta@gmail.com>
2022-12-04 00:16:10 +0400

    обновление и удаление пациента

--- src/patients/patients.controller.ts
+++ src/patients/patients.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { identity } from 'rxjs';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreatePatientDto } from './dto/create-patient.dto';
+import { UpdatePatientDto } from './dto/update-patient.dto';
 import { Patient } from './patients.model';
 import { PatientsModule } from './patients.module';
 import { PatientsService } from './patients.service';
@@ -35,6 +37,23 @@ export class PatientsController {
   getAllPatient(@Query() query: {userId:number}){
     return this.patientsService.getAllPatient(query.userId)
   }
+  @ApiOperation({summary: 'Изменение пациента'})
+  @ApiResponse({status: 200, type: Patient})
+  @Put('update')
+  async update(@Query() patient: {id:number},@Body() patientDto: UpdatePatientDto){
+    await this.patientsService.updatePatient(patientDto, patient.id);
+   return {patientDto};
+  }
+  @ApiOperation({summary: 'Удаление пациента'})
+  @ApiResponse({status: 200, type: Patient})
+  @Put('delete')
+  async delete(@Query() patient: {id:number}){
+    await this.patientsService.deletePatient(patient.id);
+   return {};
+  }
+  
 
 }
+
+
 //Лиза добавила коммент!
