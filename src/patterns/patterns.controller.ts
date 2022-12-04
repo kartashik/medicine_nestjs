2459f5bd1d91c589bf7f0de0730ee6ce59758c87
foryucheba5 <forycheba5@gmail.com>
2022-12-04 20:12:54 +0400

    Удаление и изменение шаблонов и протоколов

--- src/patterns/patterns.controller.ts
+++ src/patterns/patterns.controller.ts
@@ -1,32 +1,40 @@
-import { Body, Controller, Get, Param, Post, Query, UseGuards, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Put, Query, UseGuards, UsePipes } from '@nestjs/common';
 import { PatternsService } from './patterns.service';
 import { ValidationPipe } from "../pipes/validation.pipe";
 import { CreatePatternDto } from './dto/create-pattern.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { UpdatePatternDto } from './dto/update-pattern.dto';
 
-@ApiTags("Шаблоны")
+//@ApiTags("Шаблоны")
 @Controller('patterns')
 export class PatternsController {
     constructor(private  patternsService: PatternsService) {}
 
-    @ApiOperation({summary: 'Создание шаблона'})
+    //@ApiOperation({summary: 'Создание шаблона'})
     @UsePipes(ValidationPipe)
     @Post('create')
     create(@Body() patternDto: CreatePatternDto ){
         return this.patternsService.createPattern(patternDto)
     }
 
-    @ApiOperation({summary: 'Получение шаблона'})
+    //@ApiOperation({summary: 'Получение шаблона'})
    // @UseGuards(JwtAuthGuard)
-    @ApiQuery({name: "id", type: Number, required:true, description: "id шаблона"})
+    //@ApiQuery({name: "id", type: Number, required:true, description: "id шаблона"})
     @Get('get')
-    getPattern(@Query() query: {id:number}){
-        return this.patternsService.getPattern(query.id)
-    }
-    @Get(':id')
-    findOne(@Param('id') id: number) {
-        return this.patternsService.getPattern(id)
+    getPattern(@Query() pattern: {id:number}){
+        return this.patternsService.findOne(pattern.id)
     }
 
+    @Put('update')
+    async updatePattern(@Body() patternDto: UpdatePatternDto, @Query() pattern: {id:number}) {
+    await this.patternsService.updatePattern(patternDto, pattern.id);
+    return {patternDto};
+    }
+
+    @Put('delete')
+    async deletePattern(@Query() pattern: {id:number}) {
+    await this.patternsService.deletePattern(pattern.id);
+    return {};
+    }
 }
